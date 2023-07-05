94381190c6bad3db20b62c73a33a52fb999aba7f
noViceMin <admin@isamin.kr>
2023-07-06 00:26:59 +0900

    publish port

--- .aws/cd_scripts/start_docker.sh
+++ .aws/cd_scripts/start_docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-docker run -d
-  --name "dimigoin-back"
-  --restart always
+docker run -d \
+  --name "dimigoin-back" \
+  --restart always \
+  --publish 80:3000 \
   295965201041.dkr.ecr.ap-northeast-2.amazonaws.com/dimigoin-back:latest || exit 0
