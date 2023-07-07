c5791fc62ae39023b7e3b2728f54a6a74ce4ab11
Hyunwoo Cho <truetype24@gmail.com>
2023-07-07 14:36:34 +0900

    feat: add ClusterInfoDto

---
+++ src/common/dto/cluster.ts
@@ -0,0 +1,43 @@
+import { IsString, IsEnum } from 'nestjs-swagger-dto';
+
+export const deployment = ['prod', 'dev'] as const;
+export type Deployment = (typeof deployment)[number];
+
+export class AppInfoDto {
+  @IsString({
+    description: '프로젝트 이름',
+    example: 'Backend',
+  })
+  name: string;
+
+  @IsString({
+    description: '프로젝트 버전',
+    example: '1.0.0',
+  })
+  version: string;
+
+  @IsString({
+    description: '프로젝트 설명',
+    example: '~~~ 프로젝트의 백엔드입니다.',
+  })
+  description: string;
+
+  @IsEnum({
+    enum: { deployment } as any,
+    description: '배포 모드',
+    example: deployment[0],
+  })
+  mode: Deployment;
+
+  @IsString({
+    description: '클러스터 이름',
+    example: 'The-Life-of-Pablo.local',
+  })
+  hostname: string;
+
+  @IsString({
+    description: '작성자',
+    example: '조현우 (kanyewest@dimigo.hs.kr)',
+  })
+  author: string;
+}
